17149a3775184bfba27c33db8a1d78aa2d5e3505
Sean <sean@vestaridefair.com>
2020-12-28 23:46:46 -0800

    11:46PM ready to create the Create page...

--- src/App.js
+++ src/App.js
@@ -1,44 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import Post from './components/Post';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-const mockPosts = [
-  {
-    likes: 20,
-    description: 'The changed description',
-    image: {
-      url: '/uploads/IMG_5674_cfa4616c3b.PNG',
-    },
-  },
-  {
-    likes: 33,
-    description: 'The second post',
-    image: {
-      url: '/uploads/IMG_5674_cfa4616c3b.PNG',
-    },
-  },
-];
+import Nav from './components/Nav';
+import Home from './pages/Home';
+import Create from './pages/Create';
+import './App.css';
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  useEffect(() => {
-    const getPosts = async () => {
-      const response = await fetch('http://localhost:1337/posts');
-      const data = await response.json();
-      setPosts(data);
-    };
-    getPosts();
-  }, []);
-
   return (
     <div className='App'>
-      {posts.map((post) => (
-        <Post
-          likes={post.likes}
-          description={post.description}
-          url={post.image && post.image.url}
-        />
-      ))}
+      <h2>App</h2>
+
+      <BrowserRouter>
+        <Nav />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/create' exact component={Create} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
