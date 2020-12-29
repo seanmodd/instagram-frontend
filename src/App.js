765a1efd63d3cda1a3b463f45f7e85fae52f4fff
Sean <sean@vestaridefair.com>
2020-12-29 15:26:09 -0800

    just left question on Strapi Udemy

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Nav from './components/Nav';
 import Home from './pages/Home';
 import Create from './pages/Create';
 import SinglePost from './pages/SinglePost';
+import Login from './pages/Login';
 import './App.css';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/create' exact component={Create} />
+          <Route path='/login' exact component={Login} />
           <Route path='/:id' exact component={SinglePost} />
         </Switch>
       </BrowserRouter>
