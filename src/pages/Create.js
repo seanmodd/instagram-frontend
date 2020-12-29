6f169f82bd929ab576a6c80948f1f23f8049a271
Sean <sean@vestaridefair.com>
2020-12-29 00:15:34 -0800

    completed Section 28

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,6 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default () => 
+export default () => {
+  const [description, setDescription] = useState('');
+  const [file, setFile] = useState(null);
+  console.log('file', file);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append('data', JSON.stringify({ description }));
+    formData.append('files.image', file);
+
+    const response = await fetch('http://localhost:1337/posts', {
+      method: 'POST',
+      body: formData,
+    });
+    const data = await response.json();
+
+    console.log('data', data);
+  };
+
+  return (
     <div className='Create'>
-        Create
-    </div>;
+      <h2>Create</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder='Description'
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+        />
+        <input
+          type='File'
+          placeholder='Add a file'
+          onChange={(event) => setFile(event.target.files[0])}
+        />
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
