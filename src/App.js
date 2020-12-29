5e7557fb1486b47bace1d2132e5867f43ed6ff06
Sean <sean@vestaridefair.com>
2020-12-28 22:55:49 -0800

    starting lesson 22 to get data from API 10:55PM...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import React from 'react';
 import './App.css';
 import Post from './components/Post';
+
+const posts = [
+  {
+    likes: 20,
+    description: 'The changed description',
+    image: {
+      url: '/uploads/IMG_5674_cfa4616c3b.PNG',
+    },
+  },
+  {
+    likes: 33,
+    description: 'The second post',
+    image: {
+      url: '/uploads/IMG_5674_cfa4616c3b.PNG',
+    },
+  },
+];
 
 function App() {
   return (
     <div className='App'>
-      <Post />
+      {posts.map((post) => (
+        <Post
+          likes={post.likes}
+          description={post.description}
+          url={post.image && post.image.url}
+        />
+      ))}
     </div>
   );
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -6,89 +6,19 @@ const post = {
   description: 'Driver-side picture with schedule of deliveries!',
   likes: 20,
   author: null,
-  published_at: '2020-12-29T01:59:30.324Z',
   created_at: '2020-12-29T01:59:29.166Z',
   updated_at: '2020-12-29T01:59:30.360Z',
-  image: [
-    {
-      id: 1,
-      name: 'IMG_5674.PNG',
-      alternativeText: '',
-      caption: '',
-      width: 828,
-      height: 1792,
-      formats: {
-        thumbnail: {
-          name: 'thumbnail_IMG_5674.PNG',
-          hash: 'thumbnail_IMG_5674_cfa4616c3b',
-          ext: '.PNG',
-          mime: 'image/png',
-          width: 72,
-          height: 156,
-          size: 6.26,
-          path: null,
-          url: '/uploads/thumbnail_IMG_5674_cfa4616c3b.PNG',
-        },
-        large: {
-          name: 'large_IMG_5674.PNG',
-          hash: 'large_IMG_5674_cfa4616c3b',
-          ext: '.PNG',
-          mime: 'image/png',
-          width: 462,
-          height: 1000,
-          size: 68.21,
-          path: null,
-          url: '/uploads/large_IMG_5674_cfa4616c3b.PNG',
-        },
-        medium: {
-          name: 'medium_IMG_5674.PNG',
-          hash: 'medium_IMG_5674_cfa4616c3b',
-          ext: '.PNG',
-          mime: 'image/png',
-          width: 347,
-          height: 750,
-          size: 47.03,
-          path: null,
-          url: '/uploads/medium_IMG_5674_cfa4616c3b.PNG',
-        },
-        small: {
-          name: 'small_IMG_5674.PNG',
-          hash: 'small_IMG_5674_cfa4616c3b',
-          ext: '.PNG',
-          mime: 'image/png',
-          width: 231,
-          height: 500,
-          size: 27.61,
-          path: null,
-          url: '/uploads/small_IMG_5674_cfa4616c3b.PNG',
-        },
-      },
-      hash: 'IMG_5674_cfa4616c3b',
-      ext: '.PNG',
-      mime: 'image/png',
-      size: 71.28,
-      url: '/uploads/IMG_5674_cfa4616c3b.PNG',
-      previewUrl: null,
-      provider: 'local',
-      provider_metadata: null,
-      created_at: '2020-12-29T01:59:01.550Z',
-      updated_at: '2020-12-29T01:59:01.563Z',
-    },
-  ],
+  image: {
+    url: '/uploads/IMG_5674_cfa4616c3b.PNG',
+  },
 };
 const API_URL = 'http://localhost:1337';
 const formatImageUrl = (url) => `${API_URL}${url}`;
 
-export default () => {
-  const url = post.image[0] && post.image[0].url;
-  console.log('url', url);
-
-  const description = post.description;
-  console.log('description', description);
-  const likes = post.likes;
-  console.log('likes', likes);
+export default ({ description, likes, url }) => {
   return (
     <div className='Post'>
+      <img className='Post__Image' src={formatImageUrl(url)} />
       <h4>{description}</h4>
       <div>
         <span>Likes: {likes}</span>
